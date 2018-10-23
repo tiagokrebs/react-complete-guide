5341c0f410fba39c1e922a90bba8cb3aabb806e2
Tiago Krebs <krebstiago@gmail.com>
2018-10-23 07:18:07 -0300

    final primeira parte burger

--- react-burger-ejected/src/App.js
+++ react-burger-ejected/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 class App extends Component {
-  render () {
+  render() {
     return (
       <div>
         <Layout>
